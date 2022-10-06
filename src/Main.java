2e1b47e35f94fad4dedf0c1814af0d8249196ab4
Hamsets <romanzovut@gmail.com>
2022-10-06 16:00:09 +0300

    Домашка 12. С работы.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        Person person1 = new Person("Magnoliya", "Cvetastaya", new Address("Minsk", "Partizanskiy", 12));
+        Person person2 = new Person("Artem", "Svetlakov", new Address("Moscow", "Kutuzovsky", 2));
+        Person person3 = new Person("Svetlana", "Zvezdnaya", new Address("Minsk", "Lyubimova", 35));
+        Person person4 = new Person(null, "Pogrebnoy", new Address("Borisov", "Lenina", 24));
+        Person person5 = new Person("Ion", null, new Address("Minsk", "Aerodromnaya", 10));
+        Person person6 = new Person("Pablo", "Picasso", null);
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+        personList.add(person5);
+        personList.add(person6);
 
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    Address address;
+    public Person (String firstName, String lastName, Address address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+    @Override
+    public String toString (){
+        return firstName + " " + lastName + " " + address.getCity() + " " + address.getStreet() + " " + address.getHouse();
+    }
+}
